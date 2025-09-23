1dfe798d701308d74eb9db360af86054585ae843
Pavel Ralnikov <ralnikovp@gmail.com>
2025-09-24 00:15:05 +0300

    Add simple test

--- server/src/test/kotlin/ApplicationTest.kt
+++ server/src/test/kotlin/ApplicationTest.kt
@@ -3,6 +3,7 @@ package com.skythinkers.skynons
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlin.io.path.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,9 +14,17 @@ class ApplicationTest {
         application {
             module()
         }
-        client.post("/api/v0/simulate").apply {
-            assertEquals(HttpStatusCode.OK, status)
+
+        val config_path = Path("src/test/kotlin/config.yml")
+
+        println(config_path.readText())
+
+        val responce = client.post("/api/v0/simulate") {
+            contentType(ContentType.Application.Yaml)
+            setBody(config_path.readText())
         }
+
+        assertEquals(responce.status, HttpStatusCode.OK)
     }
 
 }
