adfb2da204d2617357e1e85dd410266eaf40a85f
Pavel Ralnikov <ralnikovp@gmail.com>
2025-09-23 23:55:14 +0300

    Add backend route

--- server/src/main/kotlin/com/skythinkers/skynons/Routing.kt
+++ server/src/main/kotlin/com/skythinkers/skynons/Routing.kt
@@ -6,13 +6,80 @@ import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.util.pipeline.*
+import io.ktor.utils.*
+import io.ktor.client.request.forms.*
+import kotlin.io.path.*
+import kotlin.io.readln
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Runtime
 
 fun Application.configureRouting() {
     routing {
         route("/api/v0") {
             post("/simulate") {
-                val text = call.receiveText()
-                call.respondText(text)
+                try {
+                    val text = call.receiveText()
+                    val tempSimulationConfig = createTempFile("config", ".yml")
+
+                    tempSimulationConfig.writeText("$text\n")
+
+                    // needs for parsing on back
+                    tempSimulationConfig.appendText(
+                        "topology_config_path: ${tempSimulationConfig.absolutePathString()}\n"
+                    )
+
+                    val tempOutputDirectory = createTempDirectory("output")
+                    val process = Runtime.getRuntime().exec(
+                        arrayOf(
+                            "../backend/build/nons",
+                            "-c",
+                            tempSimulationConfig.absolutePathString(),
+                            "--output-dir",
+                            tempOutputDirectory.absolutePathString()
+                            )
+                        )
+                    val result = process.waitFor()
+                    if (result != 0) {
+                        val stderrReader = BufferedReader(InputStreamReader(process.errorStream))
+                        val stderrLines = stderrReader.readLines()
+                        call.respond(
+                            HttpStatusCode.BadRequest,
+                            "Backend error:\n ${stderrLines.joinToString(separator="\n")}"
+                        )
+                    }
+
+                    val BASENAMES = listOf(
+                        "cwnd.svg",
+                        "packet_reordering.svg",
+                        "rate.svg",
+                        "rtt.svg"
+                    )
+
+                    val data = MultiPartFormDataContent ( formData {
+                        BASENAMES.forEach() {basename -> append(
+                            basename,
+                            Path(tempOutputDirectory.pathString, basename).readBytes(),
+                            Headers.build {
+                                append(HttpHeaders.ContentType, "image/svg")
+                                append(HttpHeaders.ContentDisposition, "filename=\"$basename\"")
+                            })
+                        }
+                    } )
+
+                    call.respond(HttpStatusCode.OK, data)
+
+                } catch (e: Exception) {
+                    call.respond(HttpStatusCode.InternalServerError, e.toString())
+                }
+
             }
         }
     }
