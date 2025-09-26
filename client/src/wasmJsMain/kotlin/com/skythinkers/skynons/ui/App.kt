145e78afccda53cb9865bcb538da1cf0873b201c
Zorsh <incognito1458@gmail.com>
2025-09-26 22:00:33 +0300

    Cleared unused imports

--- client/src/wasmJsMain/kotlin/com/skythinkers/skynons/ui/App.kt
+++ client/src/wasmJsMain/kotlin/com/skythinkers/skynons/ui/App.kt
@@ -1,10 +1,8 @@
 package com.skythinkers.skynons.ui
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -13,7 +11,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil3.ImageLoader
