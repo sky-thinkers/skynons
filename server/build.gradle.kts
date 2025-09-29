f84a4c846ecf4bddeefae608f77861566d78bdca
Leonid Ternopol <develop.firelion@gmail.com>
2025-09-29 11:14:33 +0300

    Add client API v1 impl (#23)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,9 +21,12 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.cors)
     implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.content.negotiation)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.client.core)
+    implementation(libs.ktor.serialization.json)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 
